847753c89526d9ffa15d782c8454f3b97cd11712
lrios20 <38369384+lrios20@users.noreply.github.com>
2018-10-02 22:54:48 -0500

    class react

--- SafePass/SafePass/Scripts/jsx/home.jsx
+++ SafePass/SafePass/Scripts/jsx/home.jsx
@@ -1,6 +1,23 @@
 ﻿function GetReact() {
     const elemento = (
-            <h1>Safe Pass</h1>
+            <h1>Bienvenidos a Safe Pass</h1>
     );
+    console.log("Render bienvenido" + elemento);
     ReactDOM.render(elemento, document.getElementById('app'));
 }
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+
+    console.log(props.name);
+}
+
+const el = <Welcome name="luis" />;
+ReactDOM.render(
+    el,
+    document.getElementById('root')
+);
