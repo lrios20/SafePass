72be74d5c764c412a8b40ea912a6e407ce547d54
lrios20 <38369384+lrios20@users.noreply.github.com>
2018-10-03 12:30:05 -0500

    ModelSafe + ajax dni validation
    Cree el model de la base de datos local con una sola tabla ..
    hice un demo de validacion con ajax validando dni por persona
    simulando la api de la reniec

---
+++ SafePass/SafePass/Controllers/PersonaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SafePass.Models;
+namespace SafePass.Controllers
+{
+    public class PersonaController : Controller
+    {
+        DB_safePassEntities db = new DB_safePassEntities();
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetPersonaValidation(string id = null) {
+
+            if (id != null && id != "")
+            {
+                var lista = db.tb_persona.Select(x => new
+                {
+                    x.id_persona,
+                    x.nombres,
+                    x.apellidos,
+                    x.celular,
+                    x.ciudad,
+                    x.direccion,
+                    x.dni,
+                    x.fecha_nacimiento
+                }).Where(x => x.dni == id).ToList();
+
+                if (lista.Count() == 0)
+                {
+                    return Json(lista, JsonRequestBehavior.AllowGet);
+
+                }
+                else {
+                    return Json(lista, JsonRequestBehavior.AllowGet);
+
+                }
+            }
+            else
+                return Json("error", JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+}
